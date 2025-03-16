f6a78078f0b3e680c801bbb705665fa461640020
Zaid <zaidkh1303@gmail.com>
2025-03-16 15:53:11 +0530

    some changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,9 +14,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/mohd-zaid-887207257/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/zaid1303"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/zaidkh_13/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
           </Col>
